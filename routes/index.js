2c32a51d922925432c64096954f97844c462abe4
DougSpardel <douglasspardel@gmail.com>
2024-02-06 20:53:19 -0700

    Handlebars

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 // routes
 const router = require('express').Router();
 const apiRoutes = require('./api');
-
+const homepage = require('./homepage');
 router.use('/api', apiRoutes);
-
+router.use('/', homepage);
 router.use((req, res) => {
   res.send("<h1>Wrong Route!</h1>")
 });
