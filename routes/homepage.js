b3db78c83e50ce661967d52bb6ff009de9797f92
Capti <captianbeau@gmail.com>
2024-02-09 18:43:33 -0700

    started on the home page view

--- routes/homepage.js
+++ routes/homepage.js
@@ -5,19 +5,33 @@ const router = express.Router();
 
 router.get('/', async (req, res) => {
     res.render('homepage', {
-      title:'Home Page'
+      title:'Home Page',
+      nav1:'/issues',
+      tNav1:'Issue' ,
+      nav2:'/maintenance',
+      tNav2:'Notes'
     })    
 
 });
 
 router.get('/issues', async (req,res) => {
   res.render('issues', {
-    title: 'Issue'
+    title: 'Issue',
+    nav1:'/',
+    tNav1:'Home' ,
+    nav2:'/maintenance',
+    tNav2:'Notes'
   })
 })
 
 router.get('/maintenance', (req,res) => {
-  res.render('maintenance')
+  res.render('maintenance',{
+    title:'Notes',
+    nav1:'/',
+    tNav1:'Home' ,
+    nav2:'/issues',
+    tNav2:'Issue'
+  })
 })
 
 router.get('/login', (req, res) => {
