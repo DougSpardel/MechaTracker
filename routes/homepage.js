4df84eec8d12f7c2e3409cd1630694ad705a4fac
Capti <captianbeau@gmail.com>
2024-02-10 13:45:20 -0700

    deleted code

--- routes/homepage.js
+++ routes/homepage.js
@@ -8,8 +8,7 @@ router.get('/', async (req, res) => {
       title:'Home Page',
       nav1:'/issues',
       tNav1:'Issue' ,
-      nav2:'/maintenance',
-      tNav2:'Notes'
+
     })    
 
 });
@@ -19,20 +18,11 @@ router.get('/issues', async (req,res) => {
     title: 'Issue',
     nav1:'/',
     tNav1:'Home' ,
-    nav2:'/maintenance',
-    tNav2:'Notes'
+
   })
 })
 
-router.get('/maintenance', (req,res) => {
-  res.render('maintenance',{
-    title:'Notes',
-    nav1:'/',
-    tNav1:'Home' ,
-    nav2:'/issues',
-    tNav2:'Issue'
-  })
-})
+
 
 router.get('/login', (req, res) => {
     if (req.session.loggedIn) {
